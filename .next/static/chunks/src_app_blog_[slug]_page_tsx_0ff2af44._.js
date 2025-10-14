94e58d0043fdb7fc416a0eab803c970f3e6b21a0
Kothi-0657 <service@kothiindia.com>
2025-10-14 10:37:54 +0530

    final update

--- .next/static/chunks/src_app_blog_[slug]_page_tsx_0ff2af44._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_blog_BlogPostClient_tsx_db861957._.js"
-],
-    source: "dynamic"
-});

7a38690fbf3df2b2b0296628b459b325147e8cd8
Kothi-0657 <service@kothiindia.com>
2025-10-14 12:03:34 +0530

    Remove .next cache files and update .gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"21I5pS4suziE7qJo+i/c/P15mNXku0UQ1k+hm2jc+rw=\"\n}"
--- .next/static/chunks/app/services/construction/page-96374f61ded6bd8f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[201],{5287:(e,s,_)=>{Promise.resolve().then(_.bind(_,7812))}},e=>{e.O(0,[711,733,356,812,441,255,358],()=>e(e.s=5287)),_N_E=e.O()}]);
